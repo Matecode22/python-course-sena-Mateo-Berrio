f985678b7f7894fafd35730ed5bb52db3e96b6af
Matecode22 <mateoberriov5@gmail.com>
2025-07-02 00:07:16 -0500

    python_sintaxis

---
+++ python_sintaxis.py
@@ -0,0 +1,25 @@
+# wrong way to do it
+x = 1 +   2
++ 3
+print(x)
+### The result is 3 because you only have the sum that begins with 1 and 2,/
+# while the number 3 below is considered another operation.
+
+#right way to do it 
+
+### add \
+x = 1 +   2\
++ 3
+print(x)
+
+### Correct
+name = "Sean"
+if name == "Sean":
+    print(name) #respect the line break
+
+### Incorrect
+name = "Sean"
+#if name == "Jhon": 
+#print(name)
+
+ 
