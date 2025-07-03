15e801f93aea717ffc0c8e2ba9eee430f0cc0436
Matecode22 <mateoberriov5@gmail.com>
2025-07-02 23:59:25 -0500

    dataTypes

---
+++ initial basics/python_datatypes.py
@@ -0,0 +1,19 @@
+a = 10.0
+print(type(a))
+
+b = 20,1
+print(type(b))
+
+c = "Hola soy Colommbino :)"
+print(type(c))
+
+#secuence -> String , List , Tuples
+
+list =  [10,20,30,40,50]
+
+tuple = (1,"hello",2.6,"A")
+print(tuple)
+print(tuple[1])
+
+dictionary = {'a':22 , 'b':44}
+print(dictionary['a'])
