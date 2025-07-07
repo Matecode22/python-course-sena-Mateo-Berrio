e608d438511577a4a4b1e047f4a04fa02606b00c
Matecode22 <mateoberriov5@gmail.com>
2025-07-07 12:26:33 -0500

    python_operators

--- initial basics/python_operators.py
+++ initial basics/python_operators.py
@@ -1,14 +1,31 @@
 print(2+2)
 
+print(10 - 5)
+
 print(35/2)
 
 print(35*5)
 
 a = True
-b= False
+b = True
 
 if a == b:
     print("All true!")
 
-if a and b:
+c = True
+d = False 
+if c and d:
+    print("All true!")
+
+e = True
+f = False 
+
+if e or f:
+    print("All true!")
+    
+    
+j = False   
+k = False
+
+if not(j) or not(k):
     print("All true!")
