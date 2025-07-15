6ded97bab2d4cd3e61d0cb43120a9f9a18bba5f2
Matecode22 <mateoberriov5@gmail.com>
2025-07-14 23:59:56 -0500

    pyhon_loops

--- Control flow and conditions/pyhon_match.py
+++ Control flow and conditions/pyhon_match.py
@@ -4,3 +4,4 @@ if http_status == 200 or http_status == 400 :
      print("Succes")
 elif http_status == 200:
     print("Bad request")
+    
---
+++ Control flow and conditions/python_loops.py
@@ -0,0 +1,9 @@
+
+#for i in range(10):
+#    print("result :" i)
+# 
+
+desserts = ['creme Brulee','Apple Pie','Churros','Tiramisu','Chocolate Cake']
+
+for item in desserts:
+    print("I like this desert: ", desserts)
